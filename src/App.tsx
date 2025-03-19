8f823611a35ef91007202467522a506a0b857a69
LillKonst <konstlillkristine@gmail.com>
2025-03-19 15:24:08 +0100

    treact, typescript, tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from "./images/hort1LOGO.svg";
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+
+    </div>
+      Hort1 Greenline
+    </>
+  )
+}
+
+export default App

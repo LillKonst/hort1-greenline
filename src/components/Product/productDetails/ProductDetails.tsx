4a2bf13e4deaa25215f4e512eb12ff726bc61d10
LillKonst <konstlillkristine@gmail.com>
2025-04-17 12:23:31 +0200

    added media queries and fixed footer

--- src/components/Product/productDetails/ProductDetails.tsx
+++ src/components/Product/productDetails/ProductDetails.tsx
@@ -9,19 +9,21 @@ export default function ProductDetails({ onContactClick }: ProductDetailsProps) 
         <p className="xxs:text-lg my-2">
         Dette er elsykkelen for deg som liker både byliv og natur. Med <span className="font-semibold">Hort1 <span className="text-customGreen">Greenline</span></span> får du blant annet: 
         </p>
-        <ul className=" m-3 p-2 list-disc">
-          <li className="xxs:text-lg font-semibold md:p-1">Bafang motor</li>
-          <li className="xxs:text-lg font-semibold md:p-1">65nm+ torque</li>
-          <li className="xxs:text-lg font-semibold md:p-1">Fulldempet</li>
-          <li className="xxs:text-lg font-semibold md:p-1">LED frontlys</li>
-          <li className="xxs:text-lg font-semibold md:p-1">Shimano girsystem med 27 gir</li>
-          <li className="xxs:text-lg font-semibold md:p-1">Shimano bremser</li>
+        <ul className="my-2 mx-4 p-2 xxs:text-lg xl:text-xl font-semibold md:p-1 flex flex-col gap-2">
+          <li className="before:content-['✓'] before:mr-2">Bafang motor</li>
+          <li className="before:content-['✓'] before:mr-2">65nm+ torque</li>
+          <li className="before:content-['✓'] before:mr-2">Fulldempet</li>
+          <li className="before:content-['✓'] before:mr-2">LED frontlys</li>
+          <li className="before:content-['✓'] before:mr-2">Shimano girsystem med 27 gir</li>
+          <li className="before:content-['✓'] before:mr-2">Shimano bremser</li>
         </ul>
 
-        <p className="xxs:text-lg mt-10 my-2">Kontakt oss for mer info og kjøp:</p>
+        <h2 className="font-semibold text-3xl mt-8 mx-5">23 500,-</h2>
+
+        <p className="xxs:text-lg mt-3 my-2">Kontakt oss for mer info og kjøp:</p>
         <button 
         onClick={onContactClick}
-        className="w-fit py-2 px-3 flex justify-center self-center md:self-start items-center bg-customGreen text-white rounded-md gap-2 text-center transition-transform duration-300 hover:scale-105">SEND MELDING
+        className="w-fit py-3 px-4 flex justify-center md:self-start items-center font-semibold bg-customGreen text-white rounded-md gap-2 text-center transition-transform duration-300 hover:scale-105">SEND MELDING
         </button>
     </div>
   );
--- src/components/homeBike/HomeBike.tsx
+++ src/components/homeBike/HomeBike.tsx
@@ -1,22 +1,28 @@
 import bike from "../../images/GreenlineBike.jpg";
+import { Link } from "react-router-dom";
+
+export function GreenlineProduct() {
+  return <div>GreenlineProduct</div>;
+}
 
 export default function HomeBike () {
   return (
     <div className="w-full flex justify-center">
     <div className="w-full xl:w-10/12 flex flex-col md:flex-row gap-4 mt-5 justify-center items-center">
-      <div className="w-full md:w-2/5 hover:scale-105">
-        <img src={bike} alt="sykkel" className="w-full h-full object-cover"/>
-      </div>
-      <div className="m-5 w-full md:w-3/5">
-        <h2 className="text-5xl font-semibold mt-5 text-customGreen tracking-wider">Greenline</h2>
+      <Link to="/GreenlineProduct" className="w-full md:w-2/5 hover:scale-105">
+      
+        <img src={bike} alt="sykkel" className="w-full h-full object-cover px-10 md:px-0"/>
+      </Link>
+      <div className="md:m-5 w-full md:w-3/5">
+        <h2 className="text-5xl font-semibold md:mt-5 text-customGreen tracking-wider">Greenline</h2>
         <p className="xxs:text-lg my-2 xl:text-2xl m-2">
         Dette er elsykkelen for deg som liker både byliv og natur. Med <span className="font-semibold">Hort1 <span className="text-customGreen">Greenline</span></span> får du blant annet: </p>
 
-        <ul className=" mt-10 p-2 flex gap-4 flex-wrap">
-          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-sm md:text-sm lg:text-lg font-bold text-center w-20 h-20 lg:w-24 lg:h-24 flex flex-col justify-center items-center"><p>Bafang</p><p>motor</p> </li>
-          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-sm md:text-sm lg:text-lg font-bold text-center w-20 h-20 lg:w-24 lg:h-24 flex flex-col justify-center items-center"><p>65nm+</p> <p>torque</p></li>
-          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-sm md:text-sm lg:text-lg font-bold text-center w-20 h-20 lg:w-24 lg:h-24 flex flex-col justify-center items-center"><p>Shimano</p><p>27 gir</p> </li>
-          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-sm md:text-sm lg:text-lg font-bold text-center w-20 h-20 lg:w-24 lg:h-24 flex flex-col justify-center items-center"><p>Shimano</p> <p>bremser</p></li>
+        <ul className="mt-2 md:mt-10 p-2 flex gap-2 xs:gap-4 flex-wrap">
+          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-xs xs:text-sm sm:text-lg font-bold text-center w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 flex flex-col justify-center items-center"><p>Bafang</p><p>motor</p> </li>
+          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-xs xs:text-sm sm:text-lg font-bold text-center w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 flex flex-col justify-center items-center"><p>65nm+</p> <p>torque</p></li>
+          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-xs xs:text-sm sm:text-lg font-bold text-center w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 flex flex-col justify-center items-center"><p>Shimano</p><p>27gir</p> </li>
+          <li className="bg-customGreen rounded-full hover:scale-105 p-5 text-white text-xs xs:text-sm sm:text-lg font-bold text-center w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 flex flex-col justify-center items-center"><p>Shimano</p> <p>bremser</p></li>
           
         </ul>
       </div>
